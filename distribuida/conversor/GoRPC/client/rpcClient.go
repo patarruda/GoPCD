a8b27ad45151f6e7b687bf36cd128748f81340f7
patarruda <paa3@cin.ufpe.br>
2023-08-13 18:03:43 -0300

    +EX04

--- distribuida/conversor/GoRPC/client/rpcClient.go
+++ distribuida/conversor/GoRPC/client/rpcClient.go
@@ -11,7 +11,6 @@ import (
 	"time"
 )
 
-var wg sync.WaitGroup
 var (
 	clientes       string  // quantidade total de clientes
 	clientID       int     // id do cliente atual
@@ -22,42 +21,6 @@ var (
 	csvFile  = "desempenhoRPC_clienteMedio.csv" // arquivo CSV para armazenar os resultados de desempenho
 	//csvFile = "desempenhoRPC_TempoTotal.csv" // arquivo CSV para armazenar os resultados de desempenho
 )
-
-func main_() {
-	if len(os.Args) != 2 {
-		fmt.Println("Uso: go run rpc_client.go <número_de_clientes>")
-		return
-	}
-
-	numClients, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		fmt.Println("Erro ao converter o número de clientes:", err)
-		return
-	}
-
-	//numRequests := 10000 // Número de solicitações por cliente
-
-	file, err := os.Create("desempenhoRPC.csv")
-	if err != nil {
-		fmt.Println("Erro ao criar o arquivo CSV:", err)
-		return
-	}
-	defer file.Close()
-
-	//writer = csv.NewWriter(file)
-
-	//defer writer.Flush()
-
-	//wg.Add(numClients)
-	//for i := 0; i < numClients; i++ {
-	//	go runClient(i, numRequests, writer)
-	//}
-
-	wg.Wait()
-
-	// Aguardar até que todos os clientes terminem
-	time.Sleep(time.Duration(numClients) * time.Second)
-}
 
 func main() {
 
@@ -95,50 +58,6 @@ func main() {
 	writeToCSV(record)
 	fmt.Printf("Cliente %d: TEMPO TOTAL: %s\n", clientID, tt_elapsedStr)*/
 
-}
-
-func runClient(clientID, numRequests int, writer *csv.Writer) {
-
-	defer wg.Done()
-
-	client, err := rpc.Dial("tcp", "localhost:1234")
-	if err != nil {
-		fmt.Printf("Cliente %d: Erro ao conectar ao servidor: %s\n", clientID, err)
-		return
-	}
-	defer client.Close()
-
-	for i := 0; i < numRequests; i++ {
-		//req := base.RequestConversor{
-		//	Valor:    100,
-		//	FromUnit: "C",
-		//	ToUnit:   "F",
-		//}
-		req := base.GenerateRequest("C", "F")
-
-		startTime := time.Now()
-		var reply base.RequestConversor
-		err = client.Call("ConversorService.Converter", req, &reply)
-		if err != nil {
-			fmt.Printf("Cliente %d: Erro na chamada %d: %s\n", clientID, i+1, err)
-			continue
-		}
-		fmt.Printf("Conversão %d: %.2f %s para %s => %.2f %s\n", i, req.Valor,
-			req.FromUnit, req.ToUnit, reply.Valor, reply.ToUnit)
-
-		elapsedTime := time.Since(startTime)
-
-		// Armazenar as informações no arquivo CSV
-		record := []string{
-			strconv.Itoa(clientID),
-			strconv.Itoa(i + 1),
-			elapsedTime.String(),
-		}
-		if err := writer.Write(record); err != nil {
-			fmt.Println("Erro ao escrever no arquivo CSV:", err)
-		}
-		fmt.Printf("Cliente %d: Requisição %d - RTT: %v\n", clientID, i+1, elapsedTime)
-	}
 }
 
 func ClientRPC(numRequests int, fromUnit string, toUnit string) {
